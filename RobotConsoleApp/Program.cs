4dcf3a052b7af8b990075261248871d6c4fb3247
violy <violy@DESKTOP-C8DMJ1J>
2024-02-19 00:27:56 +0700

    Добавьте файлы проекта.

---
+++ RobotConsoleApp/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using RobotLibrary;
+
+namespace RobotConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Robot robot1 = new Robot(0, 0, "Robot 1");
+            Robot robot2 = new Robot(10, 100, "Robot 2");
+
+            Thread thread1 = new Thread(new ThreadStart(() =>
+            {
+                for (int i = 0; i < 5; i++) // пусть каждый робот сделает 5 шагов
+                {
+                    robot1.Move();
+                    Console.WriteLine(robot1.GetInfo());
+                    Thread.Sleep(1000); // задержка в 1 секунду между шагами
+                }
+            }));
+
+            Thread thread2 = new Thread(new ThreadStart(() =>
+            {
+                for (int i = 0; i < 5; i++) // пусть каждый робот сделает 5 шагов
+                {
+                    robot2.Move();
+                    Console.WriteLine(robot2.GetInfo());
+                    Thread.Sleep(1000); // задержка в 1 секунду между шагами
+                }
+            }));
+
+            thread1.Start();
+            thread2.Start();
+
+            thread1.Join();
+            thread2.Join();
+
+            Console.WriteLine("Работа потоков завершена.");
+
+        }
+    }
+}
---
+++ RobotLibrary/Robot.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotLibrary
+{
+    public class Robot
+    {
+        private readonly Random _random = new Random();
+        private (int X, int Y) _coord;
+
+        public string Name { get; set; }
+        public (int X, int Y) Coordinates { get { return _coord; } }
+
+        public Robot(int x, int y, string name)
+        {
+            _coord = (x, y);
+            Name = name;
+        }
+
+        public void Move()
+        {
+            int direction = _random.Next(0, 4);
+            switch (direction)
+            {
+                case 0: // вперёд
+                    _coord.Y++;
+                    break;
+                case 1: // назад
+                    _coord.Y--;
+                    break;
+                case 2: // влево
+                    _coord.X--;
+                    break;
+                case 3: // вправо
+                    _coord.X++;
+                    break;
+            }
+        }
+
+        public string GetInfo()
+        {
+            return $"{Name} ({_coord.X}, {_coord.Y})";
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} ({_coord.X}, {_coord.Y})";
+        }
+    }
+}
